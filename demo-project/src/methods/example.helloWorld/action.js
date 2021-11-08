0f3030f94a63451f87ee7383551cc8ec9e063ee4
datthaprasad <datthaprasad29@gmail.com>
2021-11-08 12:26:11 +0530

    initial setup and helloWorld API is implemented

---
+++ demo-project/src/methods/example.helloWorld/action.js
@@ -0,0 +1,11 @@
+
+class ExampleHelloWorldAction extends baseAction {
+
+  async executeMethod() {
+    //You can set other response types as well.For that you have to deifine it in global/i18n/response.js
+    this.setResponse('SUCCESS');  
+    return "Hellow World!..";
+  };
+
+}
+module.exports = ExampleHelloWorldAction;
